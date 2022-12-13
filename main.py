2ad1665d5282f6801973fe8b20d4ed452c90c8f4
BuildTools <unconfigured@null.spigotmc.org>
2022-12-13 01:42:33 +0100

    Finished with menu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pygame
+import time
 
 
 def getclick():
@@ -14,7 +15,7 @@ def getclick():
             for actor in actors:
                 if actor["rect"].collidepoint(mouse):
                     return actor
-                    
+
 
 def ferry(who):
     done = False
@@ -34,9 +35,9 @@ def failure():
     msg_box.center = arena.center
     window.blit(msg, msg_box)
     pygame.display.flip()
-    pygame.time.wait(10000)
-    
-    
+    pygame.time.wait(1000)
+
+
 def success():
     myfont = pygame.font.Font('freesansbold.ttf', 48)
     msg = myfont.render("Win", True, (0, 255, 0))
@@ -45,21 +46,26 @@ def success():
     window.blit(msg, msg_box)
     pygame.display.flip()
     pygame.time.wait(1000)
-    
-    
+
+
 def draw_actors():
     for i, actor in enumerate(actors):
         actor["surf"] = pygame.image.load(actor["file"])
         actor["rect"] = actor["surf"].get_rect()
         actor["start"] = (0, (i+1)*arena.height/6.8)
         actor["rect"].midleft = actor["start"]
-    
+
     boat["surf"] = pygame.image.load(boat["file"])
     boat["rect"] = boat["surf"].get_rect()
     boat["rect"].bottomleft = (arena.width/8, arena.height)
 
 
-def mount(actor):  
+def mount(actor):
+    if boat["status"] == "right" and actor["rect"].left < WIDTH/2:
+        return
+    if boat["status"] == "left" and actor["rect"].right > WIDTH/2:
+        return
+
     match len(mounted):
         case 0:
             mounted.append(actor)
@@ -73,7 +79,7 @@ def mount(actor):
         case _:
             return
 
-        
+
 def unmount(actor):
     match len(mounted):
         case 0:
@@ -96,31 +102,34 @@ def unmount(actor):
 
 
 def update_screen():
-        window.blit(bg, (0, 0))
-        for actor in actors:
-            window.blit(actor["surf"], actor["rect"])
-        pygame.display.flip()   
+    window.blit(bg, (0, 0))
+    for actor in actors:
+        window.blit(actor["surf"], actor["rect"])
+    show_moves()
+    pygame.display.flip()
+
 
 def move_passengers():
     if len(mounted) == 1:
-         mounted[0]["rect"].midbottom = boat["rect"].midtop
+        mounted[0]["rect"].midbottom = boat["rect"].midtop
     elif len(mounted) == 2:
         mounted[0]["rect"].bottomleft = boat["rect"].topleft
         mounted[1]["rect"].bottomright = boat["rect"].topright
-        
+
+
 def move_boat():
     global boat
-    
+
     if len(mounted) == 0:
         return False
-    
+
     if boat["status"] == "left":
         while boat["rect"].right < 6*arena.width/8:
             boat["rect"] = boat["rect"].move(step, 0)
             move_passengers()
             update_screen()
         boat["status"] = "right"
-        
+
     elif boat["status"] == "right":
         while boat["rect"].left > arena.width/8:
             boat["rect"] = boat["rect"].move(-step, 0)
@@ -129,7 +138,7 @@ def move_boat():
         boat["status"] = "left"
     return True
 
-    
+
 def update_graph():
     global gamestate
     path = ""
@@ -141,81 +150,187 @@ def update_graph():
     gamestate = gamegraph[gamestate][path]
 
 
+def show_menu():
+    """
+    Shows the menu options and handles
+    the click event
+    """
+    global menu
+
+    # title
+    maintext = largefont.render("Missionaries and Cannibals", True, white)
+    maintext_rect = maintext.get_rect()
+    maintext_rect.center = (HEIGHT/2, WIDTH/2 - 200)
+    window.blit(maintext, maintext_rect)
+
+    # instructions
+    instr1 = smallfont.render("Click on the missionaries and "
+                              "cannibals to mount "
+                              "them on the boat", True, white)
+    instr1_rect = instr1.get_rect()
+    instr1_rect.center = (HEIGHT/2, WIDTH/2 - 50)
+    window.blit(instr1, instr1_rect)
+
+    # instructions 2
+    instr2 = smallfont.render("Click the boat to transport them", True, white)
+    instr2_rect = instr2.get_rect()
+    instr2_rect.center = (HEIGHT/2, WIDTH/2 - 15)
+    window.blit(instr2, instr2_rect)
+
+    # instructions 3
+    instr3 = smallfont.render("Ensure to have always the "
+                              "same misionaries than cannibals!", True, white)
+    instr3_rect = instr3.get_rect()
+    instr3_rect.center = (HEIGHT/2, WIDTH/2 + 15)
+    window.blit(instr3, instr3_rect)
+
+    # play text
+    playtext = largefont.render("PRESS ENTER TO PLAY", True, white)
+    playtext_rect = playtext.get_rect()
+    playtext_rect.center = (HEIGHT/2, WIDTH/2 + 270)
+    window.blit(playtext, playtext_rect)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN:
+                menu = False
+
+    pygame.display.flip()
+
+
+def show_moves():
+    text = smallfont.render(str(moves) + " move(s)", True, black)
+    text_rect = text.get_rect()
+    text_rect.center = (HEIGHT/4, 50)
+    window.blit(text, text_rect)
+
+
 if __name__ == "__main__":
+    HEIGHT = 1280
+    WIDTH = 650
     pygame.init()
-    window = pygame.display.set_mode((1280, 650))
+    window = pygame.display.set_mode((HEIGHT, WIDTH))
     arena = window.get_rect()
     bg = pygame.image.load("bg.png")
 
-
-    boat = {"file": "boat.png", "status": "left"}
-    cannibal1 = {"file": "cannibal.png", "status": "free", "type": "c"}
-    cannibal2 = {"file": "cannibal.png", "status": "free", "type": "c"}
-    cannibal3 = {"file": "cannibal.png", "status": "free", "type": "c"}    
-    missionaire1 = {"file": "missionarie.png", "status": "free", "type": "m"}
-    missionaire2 = {"file": "missionarie.png", "status": "free", "type": "m"}
-    missionaire3 = {"file": "missionarie.png", "status": "free", "type": "m"}
-
-
-    actors = [cannibal1, cannibal2, cannibal3, missionaire1, missionaire2, missionaire3, boat]
-    draw_actors()
-
-    gamegraph = {
-                "cccmmmb-": {"m": "failure", "mm": "failure", "cm": "ccmm-cmb", "c": "ccmmm-cb", "cc": "cmmm-ccb" },
-                "ccmm-cmb": {"m": "ccmmmb-c", "cm": "cccmmmb-", "c": "failure" },
-                "ccmmm-cb": { "c": "cccmmmb-" },
-                "cmmm-ccb": { "cc": "cccmmmb-", "c": "ccmmmb-c" },
-                "ccmmmb-c": {"m": "ccmm-cmb", "mm": "failure", "cm": "failure", "c": "cmmm-ccb", "cc": "mmm-cccb"},
-                "mmm-cccb": {"c": "cmmmb-cc"},
-                "cmmmb-cc": {"m": "failure", "mm": "cm-ccmmb", "cm": "failure", "c": "mmm-cccb"},
-                "cm-ccmmb": {"m": "failure", "mm": "cmmmb-cc", "cm": "ccmmb-cm", "c": "failure", "cc": "failure"},
-                "ccmmb-cm": {"m": "failure", "mm": "cc-cmmmb", "cm": "cm-ccmmb", "c": "failure", "cc": "failure"},
-                "cc-cmmmb": {"m": "failure", "mm": "ccmmb-cm", "cm": "failure", "c": "cccb-mmm", "cc": "failure"},
-                "cccb-mmm": {"c": "cc-cmmmb", "cc": "c-ccmmmb"},
-                "c-ccmmmb": {"m": "cmb-ccmm", "mm": "failure", "cm": "failure", "c": "ccb-cmmm", "cc": "cccb-mmm"},
-                "cmb-ccmm": {"m": "c-ccmmmb", "cm": "success", "c": "failure"},
-                "ccb-cmmm": {"c": "c-ccmmmb", "cc": "success"}
-                }
+    menu = True
 
     gamestate = "cccmmmb-"
     step = 1
     action = "listen"
     mounted = []
+    moves = 0
+
+    largefont = pygame.font.Font('freesansbold.ttf', 50)
+    smallfont = pygame.font.Font('freesansbold.ttf', 30)
+    white = pygame.Color("white")
+    black = pygame.Color("black")
+
+    boat = {"file": "boat.png", "status": "left"}
+    cannibal1 = {"file": "cannibal.png", "status": "free", "type": "c"}
+    cannibal2 = {"file": "cannibal.png", "status": "free", "type": "c"}
+    cannibal3 = {"file": "cannibal.png", "status": "free", "type": "c"}
+    missionaire1 = {"file": "missionarie.png", "status": "free", "type": "m"}
+    missionaire2 = {"file": "missionarie.png", "status": "free", "type": "m"}
+    missionaire3 = {"file": "missionarie.png", "status": "free", "type": "m"}
+
+    actors = [cannibal1, cannibal2, cannibal3,
+              missionaire1, missionaire2, missionaire3, boat]
+    draw_actors()
+
+    gamegraph = {
+                "cccmmmb-": {"m": "failure",
+                             "mm": "failure",
+                             "cm": "ccmm-cmb",
+                             "c": "ccmmm-cb",
+                             "cc": "cmmm-ccb"},
+                "ccmm-cmb": {"m": "ccmmmb-c",
+                             "cm": "cccmmmb-",
+                             "c": "failure"},
+                "ccmmm-cb": {"c": "cccmmmb-"},
+                "cmmm-ccb": {"cc": "cccmmmb-",
+                             "c": "ccmmmb-c"},
+                "ccmmmb-c": {"m": "ccmm-cmb",
+                             "mm": "failure",
+                             "cm": "failure",
+                             "c": "cmmm-ccb",
+                             "cc": "mmm-cccb"},
+                "mmm-cccb": {"c": "cmmmb-cc"},
+                "cmmmb-cc": {"m": "failure",
+                             "mm": "cm-ccmmb",
+                             "cm": "failure",
+                             "c": "mmm-cccb"},
+                "cm-ccmmb": {"m": "failure",
+                             "mm": "cmmmb-cc",
+                             "cm": "ccmmb-cm",
+                             "c": "failure",
+                             "cc": "failure"},
+                "ccmmb-cm": {"m": "failure",
+                             "mm": "cc-cmmmb",
+                             "cm": "cm-ccmmb",
+                             "c": "failure",
+                             "cc": "failure"},
+                "cc-cmmmb": {"m": "failure",
+                             "mm": "ccmmb-cm",
+                             "cm": "failure",
+                             "c": "cccb-mmm",
+                             "cc": "failure"},
+                "cccb-mmm": {"c": "cc-cmmmb",
+                             "cc": "c-ccmmmb"},
+                "c-ccmmmb": {"m": "cmb-ccmm",
+                             "mm": "failure",
+                             "cm": "failure",
+                             "c": "ccb-cmmm",
+                             "cc": "cccb-mmm"},
+                "cmb-ccmm": {"m": "c-ccmmmb",
+                             "cm": "success",
+                             "c": "failure"},
+                "ccb-cmmm": {"c": "c-ccmmmb",
+                             "cc": "success"}
+                }
 
     fpsClock = pygame.time.Clock()
-    
+
     while True:
-        if action == "listen":
-            click = getclick()
-            if click in actors:
-                if click is boat:
-                    action = "ferry"
-                else:
-                    if click["status"] == "mounted":
-                        unmount(click)
+
+        if menu:
+            show_menu()
+        else:
+
+            if action == "listen":
+                click = getclick()
+                if click in actors:
+                    if click is boat:
+                        action = "ferry"
                     else:
-                        mount(click)
+                        if click["status"] == "mounted":
+                            unmount(click)
+                        else:
+                            mount(click)
 
-        if action == "ferry":
-            if move_boat():
-                update_graph()
-            print(gamestate)
-            if gamestate == "failure":
-                action = "failure"
-            elif gamestate == "success":
-                action = "success"
-            else:
-                action = "listen"
+            if action == "ferry":
+                if move_boat():
+                    update_graph()
+                    moves = moves + 1
+                print(gamestate)
+                if gamestate == "failure":
+                    action = "failure"
+                elif gamestate == "success":
+                    action = "success"
+                else:
+                    action = "listen"
 
-        if action == "failure":
-            failure()
-            sys.exit()
+            if action == "failure":
+                failure()
+                sys.exit()
 
-        if action == "success":
-            success()
-            sys.exit()
-            
-        update_screen()
+            if action == "success":
+                success()
+                sys.exit()
 
-        pygame.display.flip()
-        fpsClock.tick(120)
+            update_screen()
+
+            pygame.display.flip()
+            fpsClock.tick(120)
